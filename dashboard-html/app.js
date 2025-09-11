5cdb8271586016cde691fcca88e592f819063dc9
rukayaj@gmail.com <rukayaj@gmail.com>
2025-09-11 14:36:32 +0200

    UI

--- dashboard-html/app.js
+++ dashboard-html/app.js
@@ -609,6 +609,17 @@ function openDetailModal(testId, containerType) {
             <p class="description"><i class="fas fa-sticky-note text-warning"></i> <strong>${testInfo ? testInfo.description : 'No description available'}</strong></p>
             ${testInfo && testInfo.notes ? `<p><i class="fas fa-info-circle text-primary"></i> ${testInfo.notes}</p>` : ''}
             ${testInfo && testInfo.suggested_fixes ? `<p><i class="fa-solid fa-wrench"></i></i> <strong>Suggested Fixes:</strong> ${testInfo.suggested_fixes}</p>` : ''}
+            ${testInfo && testInfo.gbif_autofix_note ? `
+                <p class="gbif-autofix-section">
+                    <svg class="gbif-logo-icon" viewBox="90 239.1 539.7 523.9" xmlns="http://www.w3.org/2000/svg">
+                        <path class="gbif-logo-svg" d="M325.5,495.4c0-89.7,43.8-167.4,174.2-167.4C499.6,417.9,440.5,495.4,325.5,495.4"></path>
+                        <path class="gbif-logo-svg" d="M534.3,731c24.4,0,43.2-3.5,62.4-10.5c0-71-42.4-121.8-117.2-158.4c-57.2-28.7-127.7-43.6-192.1-43.6
+                        c28.2-84.6,7.6-189.7-19.7-247.4c-30.3,60.4-49.2,164-20.1,248.3c-57.1,4.2-102.4,29.1-121.6,61.9c-1.4,2.5-4.4,7.8-2.6,8.8
+                        c1.4,0.7,3.6-1.5,4.9-2.7c20.6-19.1,47.9-28.4,74.2-28.4c60.7,0,103.4,50.3,133.7,80.5C401.3,704.3,464.8,731.2,534.3,731"></path>
+                    </svg>
+                    <strong>GBIF corrects (on ingestion):</strong> ${testInfo.gbif_autofix_note}
+                </p>
+            ` : ''}
         </blockquote>
         <div>
             <h6>The following fields in your data were flagged${containerType === 'validations' && testData.length > 0 ? ` <button type="button" class="btn btn-outline-dark btn-sm btn-dl-custom" onclick="downloadValidationCSV('${testId}')"><i class="fas fa-download me-1"></i>Download</button>` : ''}:</h6>
